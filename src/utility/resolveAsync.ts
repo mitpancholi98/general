2ab71b24715210f61291645d7b474c18c92270b9
Mit <mitpancholi1012@gmail.com>
2022-03-26 10:37:33 +0530

    added basic util for async resolve.

---
+++ src/utility/resolveAsync.ts
@@ -0,0 +1,12 @@
+import { AxiosResponse } from 'axios';
+
+const resolveAsync = async (promise: Promise<AxiosResponse>) => {
+  try {
+    const res = await promise;
+    return [res.data, null];
+  } catch (e) {
+    return [null, (e as any)?.response];
+  }
+};
+
+export default resolveAsync;
